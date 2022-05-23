f04e905dec34d647db020b2b85153006dcb26a2e
OyeeHashi <96408446+OyeeHashi@users.noreply.github.com>
2022-05-23 11:09:34 +0500

    Direction Definitions

--- RC_CAR.ino
+++ RC_CAR.ino
@@ -2,3 +2,15 @@
 #ifdef ESP32
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
+
+#define UP 1
+#define DOWN 2
+#define LEFT 3
+#define RIGHT 4
+#define UP_LEFT 5
+#define UP_RIGHT 6
+#define DOWN_LEFT 7
+#define DOWN_RIGHT 8
+#define TURN_LEFT 9
+#define TURN_RIGHT 10
+#define STOP 0

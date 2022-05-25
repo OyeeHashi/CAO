97863644fd01f123cb6d98e64cf191ccaaf3f804
OyeeHashi <hashimtop10@gmail.com>
2022-05-25 17:55:47 +0500

    Complete Html Page design for Controls

--- RC_CAR.ino
+++ RC_CAR.ino
@@ -29,19 +29,6 @@
 #define BACKWARD -1
 
 // RemoteXY select connection mode and include library 
-#define REMOTEXY_MODE__ESP32CORE_WIFI_POINT
-
-#include <RemoteXY.h>
-
-// RemoteXY connection settings 
-#define REMOTEXY_WIFI_SSID "RemoteXY"
-#define REMOTEXY_WIFI_PASSWORD "12345678"
-#define REMOTEXY_SERVER_PORT 6377
-
-#pragma pack(push, 1)
-uint8_t RemoteXY_CONF[] =   // 19 bytes
-  { 255,2,0,0,0,12,0,16,31,1,5,0,16,40,30,30,2,26,31 };
-
 
 //   struct {
 
@@ -85,3 +72,94 @@ struct MOTOR_PINS
   int pinIN1;
   int pinIN2;    
 };
+
+
+const char* htmlHomePage PROGMEM = R"HTMLHOMEPAGE(
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <style>
+    .arrows {
+      font-size:70px;
+      color:white;
+    }
+    .circularArrows {
+      font-size:80px;
+      color:white;
+    }
+    td {
+      /* background: rgb(0, 0, 252);; */
+      background: #000080;
+      border-radius:25%;
+      box-shadow: 5px 5px #888888;
+    }
+    td:active {
+      transform: translate(5px,5px);
+      box-shadow: none; 
+    }
+    .noselect {
+      -webkit-touch-callout: none; /* iOS Safari */
+        -webkit-user-select: none; /* Safari */
+         -khtml-user-select: none; /* Konqueror HTML */
+           -moz-user-select: none; /* Firefox */
+            -ms-user-select: none; /* Internet Explorer/Edge */
+                user-select: none; /* Non-prefixed version, currently
+                                      supported by Chrome and Opera */
+    }
+    </style>
+  </head>
+  <body class="noselect" align="center" style="background-color:white">
+     
+    <h1 style="color: teal;text-align:center;">MOTOR CONTROLLER</h1>
+    <h2 style="color: teal;text-align:center;">Wi-Fi CAR Control</h2>
+    
+    <table id="mainTable" style="width:400px;margin:auto;table-layout:fixed" CELLSPACING=10>
+      <tr>
+        <td style="background-color:white;box-shadow:none"></td>
+        <td ontouchstart='onTouchStartAndEnd("1")' ontouchend='onTouchStartAndEnd("0")'><span class="arrows" >&#8679;</span></td>
+      </tr>
+      
+      <tr>
+        <td ontouchstart='onTouchStartAndEnd("3")' ontouchend='onTouchStartAndEnd("0")'><span class="arrows" >&#8678;</span></td>
+        <td style="background-color:white;box-shadow:none"></td>   
+        <td ontouchstart='onTouchStartAndEnd("4")' ontouchend='onTouchStartAndEnd("0")'><span class="arrows" >&#8680;</span></td>
+      </tr>
+      
+      <tr>
+        <td ontouchstart='onTouchStartAndEnd("9")' ontouchend='onTouchStartAndEnd("0")'><span class="circularArrows" >&#8634;</span></td>
+
+        <td ontouchstart='onTouchStartAndEnd("2")' ontouchend='onTouchStartAndEnd("0")'><span class="arrows" >&#8681;</span></td>
+        <td ontouchstart='onTouchStartAndEnd("10")' ontouchend='onTouchStartAndEnd("0")'><span class="circularArrows" >&#8635;</span></td>
+
+      </tr>
+    
+      <tr>
+        <td style="background-color:white;box-shadow:none"></td>
+      </tr>
+    </table>
+    <script>
+      var webSocketUrl = "ws:\/\/" + window.location.hostname + "/ws";
+      var websocket;
+      
+      function initWebSocket() 
+      {
+        websocket = new WebSocket(webSocketUrl);
+        websocket.onopen    = function(event){};
+        websocket.onclose   = function(event){setTimeout(initWebSocket, 2000);};
+        websocket.onmessage = function(event){};
+      }
+      function onTouchStartAndEnd(value) 
+      {
+        websocket.send(value);
+      }
+          
+      window.onload = initWebSocket;
+      document.getElementById("mainTable").addEventListener("touchend", function(event){
+        event.preventDefault()
+      });      
+    </script>
+    
+  </body>
+</html> 
+)HTMLHOMEPAGE";
